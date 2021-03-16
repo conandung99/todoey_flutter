43c23cf073f3d5a85fa70fac2bfee989bc493467
dungbk <conandung9999@gmail.com>
2021-03-16 14:20:30 +0700

    [add] Demo GlobalState

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  void checkBoxCallback(bool value) {
+    setState(() {
+      isChecked = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: TaskCheckbox(),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkBox: isChecked,
+        toggleCheckBox: checkBoxCallback,
+      ),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
-  @override
-  _TaskCheckboxState createState() => _TaskCheckboxState();
-}
-
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBox;
+  final Function toggleCheckBox;
+  TaskCheckbox({this.checkBox, this.toggleCheckBox});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
-      value: isChecked,
-      onChanged: (value) {
-        setState(() {
-          isChecked = value;
-        });
-      },
+      value: checkBox,
+      onChanged: toggleCheckBox,
     );
   }
 }
